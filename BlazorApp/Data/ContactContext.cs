a0e11bb326e050fb2bd346068a4c6a98d5bfa962
AndresArana <ragaandres1124@gmail.com>
2022-02-17 16:50:52 -0500

    first commit

---
+++ BlazorApp/Data/ContactContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Data
+{
+
+    public class ContactContext : DbContext 
+    {
+        public ContactContext(DbContextOptions<ContactContext> options)
+        :base(options){}
+
+        public DbSet<Contact> Contacts {get; set;}
+    }
+}
